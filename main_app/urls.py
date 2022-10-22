04a1c80356e01a9c577cf1fa832aaf8e7e93d44c
Alexander Chentsov <alexgeniusman@gmail.com>
2022-10-23 02:12:22 +0400

    add 2nd api

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,13 +3,10 @@ from django.urls import path
 import main_app.views as views
 
 urlpatterns = [
-    # Test
-    path('test', views.TestView.as_view(), name='test'),
-
     # Predict
     path('predict', views.PredictView.as_view(), name='predict'),
     # History by engine
     path('get_history_by_engine', views.HistoryEngineView.as_view(), name='history_engine'),
-    # History by engine type
-    # Metrics by engine
+    # Metric by engine, datetime and phase
+    path('get_metric', views.MetricView.as_view(), name='metric'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+import json
 from pprint import pprint
 
 import requests
@@ -8,15 +9,79 @@ from rest_framework.views import APIView
 from dateutil import parser
 from main_app.models import User, Session, Prediction, Metric, MetricValue
 from project.permissions import CustomIsAuthenticated
+from django.db import transaction
 
 
-class TestView(APIView):
-    """ Test """
+class MetricView(APIView):
+    """ Metric by engine, datetime and phase """
     permission_classes = [CustomIsAuthenticated]
 
-    def post(self, request):
+    def get(self, request):
+        if not request.GET.get('session_id'):
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                'message': 'No session_id provided.',
+            })
+        if Session.objects.filter(id=request.GET.get('session_id')).exists():
+            session = Session.objects.get(id=request.GET.get('session_id'))
+            data = {}
+            for prediction in Prediction.objects.filter(session=session):
+                data[prediction.flight_phase] = {}
+
+            for prediction in Prediction.objects.filter(session=session):
+                data[prediction.flight_phase][prediction.engine_id] = []
+
+            for prediction in Prediction.objects.filter(session=session):
+                if prediction.flight_datetime not in data[prediction.flight_phase][prediction.engine_id]:
+                    data[prediction.flight_phase][prediction.engine_id].append(prediction.flight_datetime)
+
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={
+                'message': 'No session with such id.',
+            })
+
         return Response(status=status.HTTP_200_OK, data={
-            'message': 'Ok.',
+            'data': data,
+        })
+
+    def post(self, request):
+        if not request.data.get('engine_id'):
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                'message': 'No engine_id provided.',
+            })
+        if not request.data.get('flight_datetime'):
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                'message': 'No flight_datetime provided.',
+            })
+        if not request.data.get('phase'):
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                'message': 'No phase provided.',
+            })
+        data = {}
+        if Session.objects.filter(id=request.data.get('session_id')).exists():
+            session = Session.objects.get(id=request.data.get('session_id'))
+            predictions = (Prediction.objects.
+                           filter(session=session)
+                           .filter(engine_id=request.data.get('engine_id'))
+                           .filter(flight_phase=request.data.get('phase'))
+                           .filter(flight_datetime=request.data.get('flight_datetime'))
+                           )
+            for prediction in predictions:
+                metric_values = MetricValue.objects.filter(prediction=prediction)
+                for metric_value in metric_values:
+                    data[metric_value.metric_name.name] = metric_value.value
+
+                # metric_values = MetricValue.objects.filter(prediction=prediction)
+                # for metric_value in metric_values:
+                #     if metric_value.metric_name.name == request.data.get('metric'):
+
+
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={
+                'message': 'No session with such id.',
+            })
+
+        return Response(status=status.HTTP_200_OK, data={
+            'data': data,
         })
 
 
@@ -48,23 +113,6 @@ class HistoryEngineView(APIView):
                     else:
                         data[prediction.flight_phase][prediction.engine_id]['metrics'] = []
 
-            # session = Session.objects.get(id=request.GET.get('session_id'))
-            # predictions = Prediction.objects.filter(session=session)
-            # engines = {}
-            # for prediction in predictions:
-            #     engines[prediction.engine_id] = {
-            #         'metrics': {}
-            #     }
-            # for prediction in predictions:
-            #     metric_values = MetricValue.objects.filter(prediction=prediction)
-            #     for metric_value in metric_values:
-            #         metric_names = Metric.objects.filter(id=metric_value.metric_name.id)
-            #         for metric_name in metric_names:
-            #             if metric_name.name in engines[prediction.engine_id]['metrics']:
-            #                 engines[prediction.engine_id]['metrics'][metric_name.name].append(prediction.flight_phase)
-            #             else:
-            #                 engines[prediction.engine_id]['metrics'][metric_name.name] = []
-            # print(engines)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND, data={
                 'message': 'No session with such id.',
@@ -124,33 +172,36 @@ class PredictView(APIView):
     def post(self, request):
         file = request.FILES['file']
 
-        # ml_url = 'http://localhost:5000/predict'
-        ml_url = 'http://ml:8000/predict'
+        ml_url = 'http://localhost:5000/predict'
+        # ml_url = 'http://ml:8000/predict'
         response = requests.post(ml_url, files=dict(file=file))
+        # with open("temp.json", "w") as outfile:
+        #     json.dump(response.json(), outfile)
         session = Session.objects.create(
             user=request.user
         )
         # session.save()
-        for prediction in response.json():
-            prediction_db = Prediction.objects.create(
-                session=session,
-                flight_phase=prediction['flight_phase'],
-                flight_datetime=parser.parse(prediction['flight_datetime']),
-                engine_id=prediction['engine_id']
-            )
-            for metric in prediction['metrics'].items():
-                if metric[1] is not None:
-                    if not Metric.objects.filter(name=metric[0]).exists():
-                        metric_db = Metric.objects.create(
-                            name=metric[0]
+        with transaction.atomic():
+            for prediction in response.json():
+                prediction_db = Prediction.objects.create(
+                    session=session,
+                    flight_phase=prediction['flight_phase'],
+                    flight_datetime=parser.parse(prediction['flight_datetime']),
+                    engine_id=prediction['engine_id']
+                )
+                for metric in prediction['metrics'].items():
+                    if metric[1] is not None:
+                        if not Metric.objects.filter(name=metric[0]).exists():
+                            metric_db = Metric.objects.create(
+                                name=metric[0]
+                            )
+                        else:
+                            metric_db = Metric.objects.get(name=metric[0])
+                        MetricValue.objects.create(
+                            metric_name=metric_db,
+                            prediction=prediction_db,
+                            value=metric[1]
                         )
-                    else:
-                        metric_db = Metric.objects.get(name=metric[0])
-                    MetricValue.objects.create(
-                        metric_name=metric_db,
-                        prediction=prediction_db,
-                        value=metric[1]
-                    )
 
         return Response(status=status.HTTP_200_OK, data={
             'session_id': session.id,
